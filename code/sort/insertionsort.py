a659d519264d434f346e8f844abbe96d36840531
Miguel Alizo <miguelalizo@Ariannas-MacBook-Air.local>
2023-08-12 13:09:18 -0700

    Optimized insertionsort.py

--- code/sort/insertionsort.py
+++ code/sort/insertionsort.py
@@ -42,13 +42,18 @@ def insert(nums, p):
         return
     
     for i in range(p-1, -1, -1):
-        if nums[p] < nums[i]:
+        if nums[p] >= nums[i]:
+            return
+        else:
             nums[i], nums[p] = nums[p], nums[i]
             p-=1
 
 if __name__=="__main__":
     random.seed("ABC")
-    nums_test = [random.randint(0, 1000) for _ in range(15)]
+    nums_test = [random.randint(0, 1000) for _ in range(30)]
     insertion_sort(nums_test)
+    print(nums_test)
     nums_test = [0, 1, 2, 3, 4, 5, 7, 6, 8, 9]
     insertion_sort(nums_test)
+    print(nums_test)
+
