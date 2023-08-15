3a34eb71c509f5f3317ac32efb16a7c1ad4e9a44
Miguel Alizo <miguelalizo@Ariannas-MacBook-Air.local>
2023-08-14 22:47:37 -0700

    Update quicksort.py

--- code/sort/quicksort.py
+++ code/sort/quicksort.py
@@ -2,6 +2,18 @@ import random
 
 def quicksort(nums: list[int], low: int, high: int):
     """
+    In-place quicksort for an Array nums.
+    Runs on average in: O(nlogn) but worst case is quadratic
+    Space complexity: O(1)
+
+    Parameters
+    ----------
+    nums: list[int]
+        Array of numbers in to be sorted
+    low: int
+        Lower bound index of the nums subarray to be partitioned
+    high: int
+        Upper bound index of the nums subarray to be partitioned
     """
     if low<high:
         pivot_idx = partition(nums, low, high)
@@ -10,24 +22,43 @@ def quicksort(nums: list[int], low: int, high: int):
 
 def partition(nums: list[int], low: int, high: int):
     """
+    Find partition index and place all elements greater than pivot to the right of pivot,
+    and all elements less than puvot to the left.
+    Pivot is chosen as the right most element (so that it is out of the way)
+
+    Parameters
+    ----------
+    nums: list[int]
+        List of nums to be partitioned
+    low: int
+        Lower bound index of the nums subarray to be partitioned
+    high: int
+        Upper bound index of the nums subarray to be partitioned
+
+    Returns
+    -------
+    None
     """
     # choose rightmost element as pivot (this is kind of randomly choosing pivot)
     pivot = nums[high]
-    # pointer for greatest element
+
+    # pointer for the leftmost element in the list that is greater than pivot
     i = low-1
-    print("pivot: ",pivot, " nums: ",nums[low:high])
+    # look for elements smaller than pivot and if found then swap with current i
+    # note: as we go through the loop, as long as elements are all smaller than pivot then
+    # the i and j will be the same at the swap (and nothing happens)
     for j in range(low, high):
         if (nums[j]<pivot):
             i+=1
             (nums[i], nums[j]) = (nums[j], nums[i])
+
+    # swap first element greater than pivot with pivot
     nums[i+1], nums[high] = nums[high], nums[i+1]
-    print("pivot: ",pivot, " nums: ",nums[low:high+1], " idx: ",i+1)
     return i+1
 
 if __name__=="__main__":
-    random.seed("ABC")
-    # nums = [random.randint(0, 1000) for _ in range(15)]
-    nums = [3,1,2,6,4,5,3]
+    random.seed()
+    nums = [random.randint(0, 1000) for _ in range(15)]
     print(nums)
     quicksort(nums, 0, len(nums)-1)
     print(nums)
